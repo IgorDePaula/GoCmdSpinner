83d2b1838edefdfde38b7272cba4aa995f638d30
Igor de Paula <principe.borodin@gmail.com>
2022-02-03 07:46:00 -0300

    initial

---
+++ action.go
@@ -0,0 +1,50 @@
+package main
+
+import (
+	tea "github.com/charmbracelet/bubbletea"
+	"log"
+	"os"
+	"os/exec"
+)
+
+type actionDone struct{}
+type actionStep struct{}
+type Logs struct {
+	WarningLogger *log.Logger
+	InfoLogger    *log.Logger
+	ErrorLogger   *log.Logger
+}
+
+func runAction(m model) <-chan tea.Msg {
+
+	ch := make(chan tea.Msg, 0)
+
+	go func() {
+		/*time.Sleep(time.Second * 1)
+		ch <- actionStep{}
+		time.Sleep(time.Second * 2)
+		ch <- actionStep{}
+		time.Sleep(time.Second * 3)
+		ch <- actionStep{}*/
+		file, err := os.OpenFile("logs.txt", os.O_APPEND|os.O_CREATE|os.O_WRONLY, 0666)
+		if err != nil {
+			log.Fatal(err)
+		}
+		Logs := Logs{}
+		Logs.InfoLogger = log.New(file, "INFO: ", log.Ldate|log.Ltime|log.Lshortfile)
+		Logs.WarningLogger = log.New(file, "WARNING: ", log.Ldate|log.Ltime|log.Lshortfile)
+		Logs.ErrorLogger = log.New(file, "ERROR: ", log.Ldate|log.Ltime|log.Lshortfile)
+
+		cmd := exec.Command("composer", "require", "monolog/monolog")
+		cmd.Dir = ".." //diretorio anterior ao projeto
+		err = cmd.Run()
+		if err != nil {
+			log.Panic(err)
+		}
+		ch <- actionDone{}
+
+		close(ch)
+	}()
+
+	return ch
+}
---
+++ go.mod
@@ -0,0 +1,12 @@
+module GoCMDSpinner
+
+go 1.16
+
+require (
+	github.com/charmbracelet/bubbles v0.10.2
+	github.com/charmbracelet/bubbletea v0.19.3
+	github.com/charmbracelet/lipgloss v0.4.0
+	github.com/lucasb-eyer/go-colorful v1.2.0
+	github.com/muesli/reflow v0.3.0
+	github.com/muesli/termenv v0.11.0
+)
